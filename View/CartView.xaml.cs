05044577e6da15929cc187344a838eecd9433244
sanni <sanni@LAPTOP-1BQG2FKL>
2024-08-02 21:16:17 +0700

    Добавьте файлы проекта.

---
+++ View/CartView.xaml.cs
@@ -0,0 +1,18 @@
+using Windows.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace StoreApp.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CartView : Page
+    {
+        public CartView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ View/StoreView.xaml.cs
@@ -0,0 +1,18 @@
+using Windows.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace StoreApp.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StoreView : Page
+    {
+        public StoreView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
