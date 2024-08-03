144ca2633f9f668a5710c1db35d4db46bd6f0e20
sanni <sanni@LAPTOP-1BQG2FKL>
2024-08-03 12:38:22 +0700

    Добавление списка продуктов

---
+++ Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreApp.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; } // id
+        public string ProductName { get; set; } // name
+        public string ProductPrice { get; set; } //price
+        public string ProductImage { get; set; } //image
+    }
+}
